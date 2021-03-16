5c3aa03f9ef78be1f7b895f95d2d23c8d4d2aff7
Lucas Dias <lucas.dias@magically.com.br>
2021-03-16 16:52:31 -0300

    adj final

--- src/app/content/pages/home/home.page.ts
+++ src/app/content/pages/home/home.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+  treinoAdd(){
+    window.alert("Abrir novo treino")
+  }
+
+  continuarTreino(){
+    window.alert("Continuar treinando...")
+  }
+
+   open(msg: string){
+    window.alert("Abrindo " + msg)
+   }
 
 }
